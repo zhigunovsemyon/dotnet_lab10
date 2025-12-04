e1ce4078d0c21bf15cbadbdb7aeef6a4fc046590
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-12-04 15:52:01 +0300

    Добавил исключение на неправильный год

--- Movie/MovieException.cs
+++ Movie/MovieException.cs
@@ -13,3 +13,14 @@ public class InvalidMovieGenre : Exception
 	public InvalidMovieGenre (string? message) : base(message) { }
 	public InvalidMovieGenre (string? message, Exception? innerException) : base(message, innerException) { }
 }
+
+/// <summary> Ошибка неправильного года выпуска </summary>
+public class InvalidMovieYear : Exception
+{
+	public InvalidMovieYear() : base() { }
+
+	public InvalidMovieYear(string? message) : base(message) { }
+
+	public InvalidMovieYear(string? message, Exception? innerException)
+		: base(message, innerException) { }
+}
