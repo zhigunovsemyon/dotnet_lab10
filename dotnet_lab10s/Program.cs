010b0a872e9308cb0caeae235dc7d7f596c1c295
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-12-05 11:40:31 +0300

    Хватаю запросы сервером

--- dotnet_lab10s/Program.cs
+++ dotnet_lab10s/Program.cs
@@ -9,10 +9,11 @@ namespace Server;
 
 static class Program
 {
+
 	private static readonly TimeSpan _saveInterval = TimeSpan.FromSeconds(10.0);
 	private const string _filename = "movies.json";
 
-	private static readonly JsonSerializerOptions _options = new() { AllowTrailingCommas = true, WriteIndented = true};
+	private static readonly JsonSerializerOptions _options = new() { AllowTrailingCommas = true, WriteIndented = true };
 
 	/// <summary> Список фильмов </summary>
 	private static ConcurrentDictionary<string, Movie> _movieList = new();
@@ -20,12 +21,12 @@ static class Program
 	/// <summary> Адрес и порт для прослушивания </summary>
 	static readonly IPEndPoint listenTo = new IPEndPoint(IPAddress.IPv6Any, 9876);
 
-	static void Main (string[] args)
+	static void Main(string[] args)
 	{
 		LoadFile();
 		PrintMovieList();
 
-		new Task (SaveFile).Start();
+		new Task(SaveFile).Start();
 
 		var listener = new Socket(listenTo.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 		listener.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
@@ -37,27 +38,28 @@ static class Program
 			do {
 				Console.WriteLine("Ожидание соединения на {0}", listenTo);
 				var connSock = listener.Accept();
-				Console.WriteLine("Подключение от: {0}",connSock.RemoteEndPoint);
+				Console.WriteLine("Подключение от: {0}", connSock.RemoteEndPoint);
 
 
 				new Task(Connection, connSock).Start();
 			} while (true);
-		} catch { }
+		}
+		catch { }
 	}
 
 	/// <summary> Сохранение списка в файл </summary>
-	private static void SaveFile ()
+	private static void SaveFile()
 	{
 		do {
 			Thread.Sleep(_saveInterval);
 			var json = JsonSerializer.Serialize(_movieList, _options);
-			File.WriteAllText (_filename, json);
+			File.WriteAllText(_filename, json);
 			Console.WriteLine("Список сохранён в файл {0}", _filename);
 		} while (true);
 	}
 
 	/// <summary> Чтение файла со списком </summary>
-	private static void LoadFile ()
+	private static void LoadFile()
 	{
 		var json = "{}";
 		try {
@@ -69,13 +71,13 @@ static class Program
 	}
 
 	/// <summary> Вывод списка фильмов </summary>
-	private static void PrintMovieList ()
+	private static void PrintMovieList()
 	{
 		if (_movieList.IsEmpty) {
 			Console.WriteLine("Список пуст!");
 			return;
 		}
-		foreach (var (_, movieValue) in _movieList) { 
+		foreach (var (_, movieValue) in _movieList) {
 			Console.WriteLine(movieValue);
 		}
 	}
@@ -85,25 +87,44 @@ static class Program
 	/// </summary>
 	/// <param name="o"> Сокет подключения </param>
 	/// <exception cref="InvalidCastException"> При o == null или o отличном от System.Net.Sockets.Socket </exception>
-	private static void Connection (object? o)
+	private static void Connection(object? o)
 	{
 		var sock = o as Socket ?? throw new InvalidCastException("Program.Connection: o должен быть типа Socket");
-		sock.Send(Encoding.UTF8.GetBytes("Hello"));
+		try {
+			do {
+				var buf = new byte[1024];
+				var recieved = sock.Receive(buf);
+				if (0 == recieved) {
+					break;
+				}
+				WorkWithRemoteString(Encoding.UTF8.GetString(buf, 0, recieved));
+			} while (true);
+		}
+		catch (Exception ex) {
+			Console.Error.WriteLine($"Ошибка соединения: {ex.Message}");
+		}
+		finally {
+			sock.Close();
+		}
+	}
 
-		Thread.Sleep(TimeSpan.FromMinutes(1));
-		Console.WriteLine("Соединение завершено по времени");
-
-		sock.Shutdown(SocketShutdown.Both);
-		sock.Close();
+	private static void WorkWithRemoteString(string remoteString)
+	{
+		try {
+			var request = JsonSerializer.Deserialize<MovieLibrary.MovieRequest>(remoteString);
+			Console.WriteLine(request?.Movie);
+		} catch (Exception ex) {
+			Console.Error.WriteLine($"Ошибка при обработке: {ex.Message}");
+		}
 	}
 
 	/// <summary> Добавление новой записи или замена старой </summary>
 	/// <param name="movie">Новая запись</param>
 	/// <returns> true при удачном добавлении, false при ошибке </returns>
-	private static bool UpdateMovie (Movie movie)
+	private static bool UpdateMovie(Movie movie)
 	{
 		string key = movie.Title.ToLower();
-		if(_movieList.TryGetValue(key, out var oldMovie)) {
+		if (_movieList.TryGetValue(key, out var oldMovie)) {
 			_movieList.TryUpdate(key, movie, oldMovie);
 			return true;
 		}
@@ -113,7 +134,7 @@ static class Program
 	/// <summary> Добавление новой записи </summary>
 	/// <param name="movie">Новая запись</param>
 	/// <returns> true при удачном добавлении, false если такая запись уже есть </returns>
-	private static bool AddMovie (Movie movie)
+	private static bool AddMovie(Movie movie)
 	{
 		string key = movie.Title.ToLower();
 		return _movieList.TryAdd(key, movie);
@@ -122,7 +143,7 @@ static class Program
 	/// <summary> Удаление записи </summary>
 	/// <param name="movie">Запись</param>
 	/// <returns> true при удачном удалении, false при ошибке </returns>
-	private static bool DeleteMovie (Movie movie)
+	private static bool DeleteMovie(Movie movie)
 	{
 		string key = movie.Title.ToLower();
 		return _movieList.TryRemove(key, out _);
@@ -131,7 +152,7 @@ static class Program
 	/// <summary> Поиск записи </summary>
 	/// <param name="movie">Неполная запись с названием </param>
 	/// <returns> Запись при обнаружении, либо null </returns>
-	private static Movie? GetMovie (Movie movieKey)
+	private static Movie? GetMovie(Movie movieKey)
 	{
 		string key = movieKey.Title.ToLower();
 		return _movieList.TryGetValue(key, out var movie) ? movie : null;
